61ff9dc87f833401768602da917fbe33fb0784a8
99002509 <71483783+99002509@users.noreply.github.com>
2020-09-18 20:00:20 +0530

    Add files via upload

---
+++ main.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int num1=153;
+    int num2=8;
+    int result=is_armstrong(num1);
+    printf("%d",result);
+    result=is_power_of_two(num2);
+    printf("%d",result);
+    return 0;
+}
