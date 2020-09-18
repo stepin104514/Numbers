48b79707884c794d7fbbad62141ad28d8dc2229b
99002509 <71483783+99002509@users.noreply.github.com>
2020-09-18 18:11:25 +0530

    Add files via upload

---
+++ inc/number_operations.h
@@ -0,0 +1,30 @@
+/** 
+* @file number_operations.h
+* Calculator application with 4 mathematical operations
+*
+*/
+#ifndef __NUMBER_OPERATIONS_H__
+#define __NUMBER_OPERATIONS_H__
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdio_ext.h>
+#include <math.h>
+
+int is_prime(int);
+int is_even(int);
+int is_odd(int);
+int is_positive(int);
+int is_negative(int);
+int is_divisible_by(int,int);
+int calculate_sum_of_digits(int);
+int isPerfectSquare(int); 
+int is_armstrong(int);
+int find_lcm(int,int);
+int find_hcf(int,int);
+int is_palindrome(int);
+int is_power_of_two(int);
+
+
+
+#endif  /* #define __CALCULATOR_OPERATIONS_H__ */
