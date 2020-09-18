3ea59d3c860a828635b3cea646a1c7161eece4de
99002509 <71483783+99002509@users.noreply.github.com>
2020-09-18 20:00:50 +0530

    Update number_operations.c

--- src/number_operations.c
+++ src/number_operations.c
@@ -1,16 +1,6 @@
 #include<stdio.h>
 #include "number_operations.h"
 
-int main()
-{
-    int num1=153;
-    int num2=8;
-    int result=is_armstrong(num1);
-    printf("%d",result);
-    result=is_power_of_two(num2);
-    printf("%d",result);
-    return 0;
-}
 int is_even(int num)
 {
     if(num<0)
