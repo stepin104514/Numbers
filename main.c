8ca72500d3b0ffe8597b95af0214e2ed30403459
99002509 <71483783+99002509@users.noreply.github.com>
2020-09-18 20:57:22 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include<stdio.h>
 int main()
 {
+    
     int num1=153;
     int num2=8;
     int result=is_armstrong(num1);
     printf("%d",result);
-    result=is_power_of_two(num2);
-    printf("%d",result);
+    switch(result)
+    {
+        case 1:
+        printf("True");
+        break;
+        case 0:
+        printf("False");
+        break;
+        case -1:
+        print("Not applicable");
+        break;
+    }
     return 0;
 }
