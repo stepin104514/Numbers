e7ed8c753bb461522444e2da327b39dee3872ea4
99002509 <71483783+99002509@users.noreply.github.com>
2020-09-19 09:09:19 +0530

    Add files via upload

---
+++ 3_Implementation/src/number_operations.c
@@ -0,0 +1,225 @@
+#include<stdio.h>
+#include "number_operations.h"
+
+int is_even(int num)
+{
+    if(num<0)
+    {
+        return -1;
+    }
+    if(num%2==0)
+        return 1;
+    else
+        return 0;
+}
+int is_odd(int num)
+{
+    if(num<0)
+    {
+        return -1;
+    }
+    if(num%2!=0)
+        return 1;
+    else
+        return 0; 
+}
+
+int is_positive(int num)
+{
+    if(num>0)
+        return 1;
+    else if(num==0)
+        return -1;
+    else
+        return 0;
+      
+}
+int is_negative(int num)
+{
+  if(num<0)
+        return 1;
+    else if(num==0)
+        return -1;
+    else
+        return 0;  
+}
+int is_divisible_by(int num1,int num2 )
+{
+    if(num1<0||num2<0)
+    {
+        return -1;
+    }
+    if(num1 % num2 == 0)
+        return 1;
+    else
+        return 0; 
+}
+int calculate_sum_of_digits(int num)
+{
+    if(num<0)
+    {
+        num=-num;
+    }
+    int sum=0;
+    while(num!=0)
+    {
+        sum+= num % 10;
+        num/=10;
+    }
+    return sum;
+}
+
+int is_prime(int num)
+{
+    if(num<=0)
+    {
+        return -1;
+    }
+    int count=0;
+    for(int i=2;i<num;i++)
+    {
+        if(num%i==0)
+            {
+                count++;
+                break;
+            }
+
+    }
+    if (count == 0)
+        return 1;
+    else
+        return 0;
+    
+}
+int isPerfectSquare(int num) 
+{ 
+    if(num<0)
+    {
+        return -1;
+    }
+    for (int i = 1; i * i <= num; i++) { 
+  
+        // If (i * i = num) 
+        if ((num % i == 0) && (num / i == i)) { 
+            return 1; 
+        } 
+    } 
+    return 0; 
+} 
+int is_armstrong(int num)
+{
+    if(num<0)
+    {
+        return -1;
+    }
+    int copy=num;
+    int digit,sum=0;
+    num=copy;
+    while(num!=0)
+    {
+        digit=num%10;
+        sum+=digit*digit*digit;
+        num=num/10;
+    }
+    if(sum==copy)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+int find_lcm(int num1,int num2)
+{
+    if(num1<0||num2<0)
+    {
+        return -1;
+    }
+int max = (num1 > num2) ? num1 : num2;
+
+    while (1) {
+        if (max % num1 == 0 && max % num2 == 0) {
+            break;
+        }
+        ++max;
+    }
+    return max;
+}
+int find_hcf(int num1,int num2)
+{
+    if(num1<0||num2<0)
+    {
+        return -1;
+    }
+    int gcd;
+    for(int i=1; i <= num1 && i <= num2; ++i)
+    {
+        // Checks if i is factor of both integers
+        if(num1%i==0 && num2%i==0)
+            gcd = i;
+    }
+    return gcd;
+}
+
+int is_palindrome(int num)
+{
+    if(num<0)
+    {
+        return -1;
+    }
+    int temp=num,remainder,reverse_num=0;
+    while(temp!=0)
+    {
+      remainder=temp%10;
+      reverse_num=reverse_num*10+remainder;
+      temp/=10;
+    } 
+    if(reverse_num==num) 
+      return 1;
+    else
+      return 0;
+}
+int is_power_of_two(int num)
+{ 
+    if(num<=0) 
+        return -1; 
+  
+    int tempNum=num,flag=0;
+    
+    /*check power of two*/
+    while(tempNum!=1)
+    {
+        if(tempNum%2!=0){
+            flag=1;
+            break;
+        }
+        tempNum=tempNum/2;
+    }
+    if(flag==0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+    
+   
+} 
+int find_remainder(int num1,int num2)
+{
+    if(num1<=0 || num2 < 0) 
+        return -1; 
+    return num1 % num2;  
+
+}
+int find_sum_of_natural_nos(int num)
+{
+    int sum=0;
+    for (int i = 1; i <= num; ++i) {
+        sum += i;
+    }
+
+    return sum;
+}
---
+++ 3_Implementation/test/testnumber_operations.c
@@ -0,0 +1,178 @@
+#include <CUnit/Basic.h>
+#include <CUnit/CUnit.h>
+
+/* Modify these two lines according to the project */
+#include <number_operations.h>
+#define PROJECT_NAME    "number_operaions"
+
+/* Prototypes for all the test functions */
+void test_palindrome(void);
+void test_even(void);
+void test_prime(void);
+void test_odd(void);
+void test_positive(void);
+void test_negative(void);
+void test_divisible(void);
+void test_sum_digits(void);
+void test_perfect_square(void);
+void test_armstrong(void);
+void test_lcm(void);
+void test_hcf(void);
+void test_power_of_two(void);
+void test_remainder(void);
+void test_sum_nat(void);
+
+
+/* Start of the application test */
+int main() {
+/* Note: Do not edit START*/
+  /*Initialize and setup the Test Framework */
+  if (CUE_SUCCESS != CU_initialize_registry())
+    return CU_get_error();
+  CU_pSuite suite = CU_add_suite(PROJECT_NAME, 0, 0);
+/* Note: Do not edit END */
+  
+  
+  /* Add your test functions in this format for testing*/
+ 
+  CU_add_test(suite, "prime", test_prime);
+  CU_add_test(suite, "even", test_even);
+  CU_add_test(suite, "prime", test_palindrome);
+  CU_add_test(suite, "odd", test_odd);
+  CU_add_test(suite, "positive", test_positive);
+  CU_add_test(suite, "negative", test_negative);
+
+  CU_add_test(suite, "divisible", test_divisible);
+  CU_add_test(suite, "sum_of_digits", test_sum_digits);
+  CU_add_test(suite, "perfect_square", test_perfect_square);
+  CU_add_test(suite, "armstrong", test_armstrong);
+  CU_add_test(suite, "lcm", test_lcm);
+  CU_add_test(suite, "hcf", test_hcf);
+  CU_add_test(suite, "power_of_two", test_power_of_two);
+  CU_add_test(suite, "remainder", test_remainder);
+  CU_add_test(suite, "Sum_nat_nos", test_sum_nat);
+  
+  
+
+
+/* Note: Do not edit START*/
+  /* Setup Test Framework to output the result to Screen */
+  CU_basic_set_mode(CU_BRM_VERBOSE);
+  
+  /* run the unit test framework*/
+  CU_basic_run_tests();
+  
+  /* Cleaning the Resources used by Unit test framework */
+  CU_cleanup_registry();
+/* Note: Do not edit END */
+  return 0;
+}
+
+/* Write all the test functions */ 
+void test_palindrome(void) {
+  
+  CU_ASSERT(1 == is_palindrome(515));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == is_palindrome(13));
+}
+
+void test_even(void) {
+  CU_ASSERT(0 == is_even(3));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == is_even(12));
+}
+void test_prime(void)
+{CU_ASSERT(1 == is_prime(5));
+  
+  /* Dummy fail*/
+  CU_ASSERT(-1 == is_prime(0));
+}
+
+void test_odd(void)
+{
+	CU_ASSERT(1 == is_odd(515));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == is_odd(14));
+}
+
+void test_positive(void){
+	CU_ASSERT(1 == is_positive(515));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == is_positive(-13));
+}
+
+void test_negative(void){
+	CU_ASSERT(1 == is_negative(-515));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == is_negative(13));
+}
+
+void test_divisible(void){
+	CU_ASSERT(1 == is_divisible_by(5,5));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == is_divisible_by(5,10));
+	
+}
+
+void test_sum_digits(void){
+	CU_ASSERT(11 == calculate_sum_of_digits(515));
+  
+  /* Dummy fail*/
+  CU_ASSERT(4 == calculate_sum_of_digits(13));
+}
+
+void test_perfect_square(void){
+	CU_ASSERT(1 == isPerfectSquare(25));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == isPerfectSquare(13));
+}
+
+void test_armstrong(void)
+{
+	CU_ASSERT(1 == is_armstrong(153));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == is_armstrong(13));
+}
+
+void test_lcm(void){
+	CU_ASSERT(10 == find_lcm(5,10));
+  
+  /* Dummy fail*/
+  CU_ASSERT(2 == find_lcm(1,2));
+}
+
+void test_hcf(void){
+	CU_ASSERT(1 == find_hcf(1,515));
+  
+  /* Dummy fail*/
+  CU_ASSERT(13 == find_hcf(13,26));
+}
+
+void test_power_of_two(void){
+	CU_ASSERT(1 == is_power_of_two(8));
+  
+  /* Dummy fail*/
+  CU_ASSERT(0 == is_power_of_two(13));
+}
+
+void test_remainder(void){
+	CU_ASSERT(0 == find_remainder(10,5));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == find_remainder(25,12));
+}
+void test_sum_nat(void){
+	CU_ASSERT(3 == find_sum_of_natural_nos(2));
+  
+  /* Dummy fail*/
+  CU_ASSERT(1 == find_sum_of_natural_nos(1));
+}
+	
