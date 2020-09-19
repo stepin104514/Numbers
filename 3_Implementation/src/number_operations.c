bdd0f6837e82cb6a393702874808d4efacc9e5b7
99002509 <71483783+99002509@users.noreply.github.com>
2020-09-19 09:49:20 +0530

    Update number_operations.c

--- 3_Implementation/src/number_operations.c
+++ 3_Implementation/src/number_operations.c
@@ -209,7 +209,7 @@ int is_power_of_two(int num)
 } 
 int find_remainder(int num1,int num2)
 {
-    if(num1<=0 || num2 < =0) 
+    if(num1<=0 || num2<=0) 
         return -1; 
     return num1 % num2;  
 
